1ed2c2385a42a53f3cb5cc4f7479cccd3fab8ffd
Tanner Polera <tpolera@uncc.edu>
2014-03-09 12:06:07 -0400

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
+require 'greeter'
+
 # Default is "World"
-# Author: Tanner Polera (tpolera@uncc.edu)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
